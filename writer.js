9d21205eab80365f6591a4e039a5ee9a96ca11d9
nstrimple <97196951+nstrimple@users.noreply.github.com>
2022-01-06 22:58:05 -0500

    added the writer backed

---
+++ writer.js
@@ -0,0 +1,27 @@
+const writeFunc = () => {
+    const eng = document.getElementById('eng')
+    const port = document.getElementById('port')
+    const apPost = async () => {
+        try {
+        const response = await fetch('https://react-flashcards-70208-default-rtdb.firebaseio.com/flashcards.json', {
+            method: 'POST',
+            body: JSON.stringify({english: eng,
+            portuguese: port}),
+            headers: {
+                'content-type' : 'application/json'
+            }
+        })
+        if (!response.ok) {
+            throw new Error('Fuck')
+        }
+        const data = response.json();
+        console.log(`${data.name} witten to database`)
+        }
+        catch(error) {
+            console.log(error.message || 'Something went wrong')
+        }
+    }
+    apPost();
+}
+
+document.getElementById('sub').addEventListener('click', writeFunc);
