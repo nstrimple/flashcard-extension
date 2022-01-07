d9b59e3341f0becaa4b577b353aec9eb3d2b0dff
nstrimple <97196951+nstrimple@users.noreply.github.com>
2022-01-06 23:03:57 -0500

    fixing bugs

--- writer.js
+++ writer.js
@@ -1,27 +1,28 @@
 const writeFunc = () => {
-    const eng = document.getElementById('eng')
-    const port = document.getElementById('port')
-    const apPost = async () => {
-        try {
-        const response = await fetch('https://react-flashcards-70208-default-rtdb.firebaseio.com/flashcards.json', {
-            method: 'POST',
-            body: JSON.stringify({english: eng,
-            portuguese: port}),
-            headers: {
-                'content-type' : 'application/json'
-            }
-        })
-        if (!response.ok) {
-            throw new Error('Fuck')
+  const eng = document.getElementById("eng").value;
+  const port = document.getElementById("port").value;
+  const apPost = async () => {
+    try {
+      const response = await fetch(
+        "https://react-flashcards-70208-default-rtdb.firebaseio.com/flashcards.json",
+        {
+          method: "POST",
+          body: JSON.stringify({ english: eng, portuguese: port }),
+          headers: {
+            "content-type": "application/json",
+          },
         }
-        const data = response.json();
-        console.log(`${data.name} witten to database`)
-        }
-        catch(error) {
-            console.log(error.message || 'Something went wrong')
-        }
+      );
+      if (!response.ok) {
+        throw new Error("Fuck");
+      }
+      const data = await response.json();
+      console.log(`${data.name} witten to database`);
+    } catch (error) {
+      console.log(error.message || "Something went wrong");
     }
-    apPost();
-}
+  };
+  apPost();
+};
 
-document.getElementById('sub').addEventListener('click', writeFunc);
+document.getElementById("sub").addEventListener("click", writeFunc);
